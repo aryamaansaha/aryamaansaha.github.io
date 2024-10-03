d20a63d7e9d5686b2f058ff8e20d1db9f38c48aa
Aryamaan Saha <aryamaansaha78@gmail.com>
2024-10-03 15:19:37 -0400

    test front end setup

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple script to display an alert when clicking on the contact section
+document.getElementById("contact").addEventListener("click", function() {
+    alert("Thanks for visiting my site! Feel free to reach out.");
+  });
+  
