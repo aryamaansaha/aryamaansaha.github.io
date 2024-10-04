ae4dcd700c2d815fa767cd30816325f69557d8f0
Aryamaan Saha <aryamaansaha78@gmail.com>
2024-10-03 21:11:06 -0400

    Updates

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple script to display an alert when clicking on the contact section
-document.getElementById("contact").addEventListener("click", function() {
-    alert("Thanks for visiting my site! Feel free to reach out.");
-  });
-  
